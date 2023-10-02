3aca952960183386e7572394c7ce627e4e9352b1
James Hunter <jamessahunter@gmail.com>
2023-10-02 16:28:27 -0700

    add comments and clean up code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,53 +1,24 @@
-//start screen with button done
-//eventlisten on button done 
-//when button clicked start quiz done 
-//timer counts down one second itervals done
-//eventlisten on each answer done 
-//if right new question done 
-//if wrong subtract from time and next question done
-//game over when time =0 go to high score screen done
-//game over if end of questions done 
-//high score screen box for user to enter intitals done
-//save to local storage activity 26 done
-//records high score done
-
-
-// put question and answers in obejct
-//put all questions in array
-//show and remove text based of button presses
-
+// selects all classes and ids from html
 var startQuiz=document.querySelector("#start-quiz");
-
 var startButton=document.querySelector(".start-button");
-
 var questionSection=document.querySelector("#questions");
-
-var highScoreSection=document.querySelector("#highscore");
-
-var scoreScreen=document.querySelector("#score-screen")
-
+var scoreSection=document.querySelector("#score-section");
+var seeScores=document.querySelector("#see-scores")
 var answersId=document.querySelector("#answers");
-
 var timeEl=document.querySelector(".timer");
-
 var scoreList=document.querySelector("#scores");
-
+// creates and initilizes some variables
 var saveButton;
-
 var replayButtonId;
-
 var initialsInput;
-
 var initials=[];
-
 var scores=[];
-
 var scoreCorrect=0;
-
 var questionCounter=0;
-
 var secondsLeft=75;
-
+var correctAnswers=[3,2,0,0,1,2];
+var timeInterval;
+// creates an object for the questions
 var questions={
     q1:{question1:"Which of theses is not a git command?",
     answers:["git pull","git add","git commit","git back"],
@@ -68,256 +39,239 @@ var questions={
     asnwers:["string","bigint","char","symbol"],
     },
 }
-var correctAnswers=[3,2,0,0,1,2];
 
+// uses the questions object to create an array of the questions and answers
 var questionsArr=[[questions.q1.question1,questions.q1.answers],[questions.q2.question2,questions.q2.asnwers],
 [questions.q3.question3,questions.q3.answers],[questions.q4.question4,questions.q4.asnwers],
 [questions.q5.question5,questions.q5.answers],[questions.q6.question6,questions.q6.asnwers]];
 
-var timeInterval;
-
-
-// console.log(questionsArr[0]);
-// console.log(questionsArr);
-
-scoreScreen.addEventListener("click",function(){
-    var noScore=1;
-    highScore(noScore);
-})
-
-// console.log(questionSection);
-startButton.addEventListener("click",function(event){
-    // event.stopPropagation();
-    // mainTag.setAttribute("style","color:blue");
-    scoreScreen.style.display="none";
-    startQuiz.style.display="none";
-    
-    // startQuiz.textContent="";
-    setTime();
-    displayQuestions();
-})
-
-answersId.addEventListener("click",function(event){
-    var element=event.target;
-    if(element.matches("li")){
-        // console.log(element.getAttribute("data-number"))
-        questionCounter++;
-        displayQuestions();
-    }
-
-    checkAnswer(element.getAttribute("data-number"));
-
-
-})
-
-function displayQuestions() {
-    questionSection.style.display="block";
-
-    // console.log("test")
-    // console.log(test);
-    if (questionCounter>5){
-        highScore();
-        return;
-    }
-    // console.log(questionCounter)
-    questionSection.children[0].textContent=questionsArr[questionCounter][0];
-    for (let i = 0; i < 4; i++) {
-
-        questionSection.children[1].children[i].textContent=[i+1]+". "+ questionsArr[questionCounter][1][i];
-    
-    }
-    // console.log(answer);
-}
-
-function setTime(){
-    timeEl.textContent="Time Remaining: " +secondsLeft;
-    timeInterval =setInterval(function(){
-        secondsLeft--;
-        timeEl.textContent="Time Remaining: " +secondsLeft;
-
-        if(secondsLeft<=0){
-            clearInterval(timeInterval);
-            console.log("out of time")
-            highScore();
-        }
-    },1000);
-}
-
-function highScore(check){
-    // console.log("high score")
-    var h1=document.createElement('h1');
-    h1.textContent="Scores";
-    highScoreSection.appendChild(h1);
-
-    timeEl.textContent="";
-    questionSection.style.display="none";
-    scoreScreen.style.display="none";
-    startQuiz.style.display="none";
-    if(check){
-        console.log("works")
-        storeScores();
-        renderScores();
-    }
-    else{
-    clearInterval(timeInterval);
-    // console.log(secondsLeft);
-
-    // questionSection.children[0].textContent="";
-    // for (let i = 0; i < 4; i++) {
-    //     questionSection.children[1].children[i].textContent="";
-    // }
-    scoresText=secondsLeft+scoreCorrect;
-
-    var p=document.createElement('p');
-    var h2=document.createElement('h2');
-
-    h2.textContent="Your score is "+ scoresText
-    p.textContent="Enter your Initials and hit save to have your score added to the scoreboard."
-
-    highScoreSection.appendChild(h2);
-    highScoreSection.appendChild(p);
-    // questionSection.textContent="";
-    const form = document.createElement('form');
-      
-      // Add input fields to the form
-      form.innerHTML = `
-        <label for="initials">Initials:</label>
-        <input type="text" id="initials" name="initials" placeholder="JH"/>
-        <button id="save">Save</button>
-      `;
-
-      // Append the form to the form container
-      highScoreSection.appendChild(form);
-
-      saveButton=document.querySelector("#save");
-
-      initialsInput=document.querySelector("#initials");
-      storeScores();
-      renderScores();
-    //   console.log(saveButton);
-      saveButton.addEventListener("click",function(event){
-        event.preventDefault();
-        initialsText = initialsInput.value;
-        if(initialsText===""){
-            return;
-        }
-        // localStorage.setItem("initials",initials);
-        scores.push(scoresText);
-        initials.push(initialsText);
-        initialsInput.value="";
-        // console.log(scores);
-        storeScores();
-        // renderScores();
-        appendScore();
-    })
-    }
-    replay();
-
-
-}
-
-function replay(){
-    var replayButton=document.createElement("section")
-    replayButton.innerHTML='<button id="replay">Replay</button>';
-    highScoreSection.appendChild(replayButton);
-    replayButtonId=document.querySelector("#replay");
-    replayButtonId.addEventListener("click",function(event){
-        event.preventDefault();
-        highScoreSection.textContent="";
-        scoreList.textContent="";
-        console.log("works")
-        scoreCorrect=0;
-        secondsLeft=75;
-        questionCounter=0;
-        questionSection.style.display="block"
-        setTime();
-        displayQuestions();
-        
-
-    })
-
-}
-
-
-function appendScore(){
-    // console.log("replace ")
-    if(scores.length===1){
-        // console.log("null")
-        var score=scores[0];
-        var initial=initials[0];
-        var li=document.createElement("li")
-        li.textContent=initial +" "+score;
-        // console.log("initial" + initial);
-        scoreList.appendChild(li);
-    }
-    else{
-        console.log("length " +initials.length);
-
-    // console.log(scores);
-    // console.log(initials);
-    var score=scores[initials.length-1];
-    var initial=initials[initials.length-1];
-    var li=document.createElement("li")
-    // console.log(initial +" "+score);
-    li.textContent=initial +" "+score;
-    scoreList.appendChild(li);
-}
-
-// console.log("replace ")
-// console.log(scoreList);
-}
-
-
-function renderScores(){
-
-    for (var i = 0; i < initials.length; i++) { 
-        // li.textContent="";
-        var score=scores[i];
-        var initial=initials[i];
-        var li=document.createElement("li")
-        li.textContent=initial +" "+score;
-        // console.log("initial" + initial);
-        scoreList.appendChild(li);
-    }
-    // console.log("render ")
-    // console.log(scoreList);
-
-}
-
-function checkAnswer(answer){
-    // var number=element.getAttribute("data-number");
-    // console.log("works");
-    // console.log("counter "+ questionCounter);
-    // console.log( answer);
-    // console.log("correct "+correctAnswers[questionCounter-1]);
-    if(answer==correctAnswers[questionCounter-1]){
-        scoreCorrect+=10;
-        console.log(scoreCorrect);
-    }
-    else{
-        console.log("wrong");
-        secondsLeft -= 10;
-    }
-
-
-}
-
-
-init();
-
+// initial funtion to start code
 function init(){
+    //hides questions section
     questionSection.style.display="none";
-
+    //creates and initilizes the storage varaibles and retrives them from storage
+    //parses the strings into arrays
     var storedinitials=JSON.parse(localStorage.getItem("initials"));
     var storedScores=JSON.parse(localStorage.getItem("scores"));
-
+    //if there are storeded scores it sets them equal to there counter parts
     if (storedScores !==null){
         scores=storedScores;
         initials=storedinitials
     }   
 }
 
+// adds and event listener to the see score text 
+seeScores.addEventListener("click",function(){
+    //means the user will only be able to see the scores not enter a score
+    var noScore=1;
+    //calls scoreScreen function
+    scoreScreen(noScore);
+})
+
+// events listener for the start button
+startButton.addEventListener("click",function(event){
+    // stops displaying the start quiz info and see scores text
+    seeScores.style.display="none";
+    startQuiz.style.display="none";
+    
+    //calls set time and display questions funtion
+    setTime();
+    displayQuestions();
+})
+
+//displays the questions and answers
+function displayQuestions() {
+    //allows the question section to be seen
+    questionSection.style.display="block";
+    //checks if all questions have been answered
+    if (questionCounter>5){
+        //calls score screen function
+        scoreScreen();
+        return;
+    }
+    //displays the questions and there corresponding answers
+    questionSection.children[0].textContent=questionsArr[questionCounter][0];
+    for (let i = 0; i < 4; i++) {
+        questionSection.children[1].children[i].textContent=[i+1]+". "+ questionsArr[questionCounter][1][i]; 
+    }
+}
+
+//sets the time 
+function setTime(){
+    //sets up text next to remaining time
+    timeEl.textContent="Time Remaining: " +secondsLeft;
+    //function to count down every second
+    timeInterval =setInterval(function(){
+        //reduces time by one second
+        secondsLeft--;
+        //displays time
+        timeEl.textContent="Time Remaining: " +secondsLeft;
+        //checks if time has reached or exceeded zero
+        if(secondsLeft<=0){
+            scoreScreen();
+        }
+    },1000);
+}
+
+//event listener for a click on any answer
+answersId.addEventListener("click",function(event){
+    //retrieves what the element that was clicked
+    var element=event.target;
+    //checks the elememnt clicked is a li
+    if(element.matches("li")){
+        // increases the counter by one
+        questionCounter++;
+        //calls the display question
+        displayQuestions();
+    }
+    //calls check answer function
+    checkAnswer(element.getAttribute("data-number"));
+})
+
+//function to check the answers
+function checkAnswer(answer){
+    //checks to see if the clicked answer if correct comparing it to the array
+    if(answer==correctAnswers[questionCounter-1]){
+        //adds 10 to the score
+        scoreCorrect+=10;
+    }
+    else{
+        //decreases the time by 10 seconds for a wrong answer
+        secondsLeft -= 10;
+    }
+}
+
+//displays the scores
+function scoreScreen(check){
+    //inserts header
+    var h1=document.createElement('h1');
+    h1.textContent="Scores";
+    scoreSection.appendChild(h1);
+    // clears any other text
+    timeEl.style.display="none";
+    questionSection.style.display="none";
+    seeScores.style.display="none";
+    startQuiz.style.display="none";
+    // checks for score screen button pressed
+    if(check){
+        //retrives and renders scores
+        storeScores();
+        renderScores();
+    }
+    else{
+          //stops the timer from running
+        clearInterval(timeInterval);
+        //adds the remaining time to the correct score
+        scoresText=secondsLeft+scoreCorrect;
+        //creates elements and appends them to the page
+        var p=document.createElement('p');
+        var h2=document.createElement('h2');
+        h2.textContent="Your score is "+ scoresText
+        p.textContent="Enter your Initials and hit save to have your score added to the scoreboard."
+        scoreSection.appendChild(h2);
+        scoreSection.appendChild(p);
+        //creates a form
+        var form = document.createElement('form');
+        // Add input fields to the form
+        form.innerHTML = `
+            <label for="initials">Initials:</label>
+            <input type="text" id="initials" name="initials" placeholder="JH"/>
+            <button id="save">Save</button>
+        `;
+        // Append the form to the page
+        scoreSection.appendChild(form);
+        // selects classes for newly created elements
+        saveButton=document.querySelector("#save");
+        initialsInput=document.querySelector("#initials");
+        //retrives and renders scores
+        storeScores();
+        renderScores();
+        //adds an event listener to the save button
+        saveButton.addEventListener("click",saveButtonClicked)
+    }
+    //calls the replay function
+    replay();
+}
+
+//converts the arrays into strings
 function storeScores(){
     localStorage.setItem("scores",JSON.stringify(scores));
     localStorage.setItem("initials",JSON.stringify(initials));
 }
+
+//renders the scores
+function renderScores(){
+    //iterates through the string and creates a list item and appends it to the page
+    for (var i = 0; i < initials.length; i++) { 
+        var score=scores[i];
+        var initial=initials[i];
+        var li=document.createElement("li")
+        li.textContent=initial +" "+score;
+        scoreList.appendChild(li);
+    }
+
+}
+
+//if the save button has been clicked
+function saveButtonClicked(event){
+    //prevents default form from being used
+    event.preventDefault();
+    //grabs the initials input
+    initialsText = initialsInput.value;
+    //checks to see if something has been entered into the box
+    if(initialsText===""){
+        return;
+    }
+    //pushes the initials and scores the their respective arrays
+    scores.push(scoresText);
+    initials.push(initialsText);
+    //resets the intials input values
+    initialsInput.value="";
+    //calls retrieve scores and append scores
+    storeScores();
+    appendScore();
+}
+
+//apends the new score to the page
+function appendScore(){
+    //takes the newlyy entered score and adds it to the end of the list
+    var score=scores[initials.length-1];
+    var initial=initials[initials.length-1];
+    var li=document.createElement("li")
+    li.textContent=initial +" "+score;
+    scoreList.appendChild(li);
+}
+
+//replay functioms
+function replay(){
+    //creates a new section insided of the score sections and appends the replay button
+    var replayButton=document.createElement("section");
+    replayButton.innerHTML='<button id="replay">Replay</button>';
+    scoreSection.appendChild(replayButton);
+    replayButtonId=document.querySelector("#replay");
+    //creates an event listener on the replay button
+    replayButtonId.addEventListener("click",replayClicked)
+}
+
+//function for replay button clicked on
+function replayClicked(event){
+    //resets the score and text content so it doesn't get duplicated when the score screen apears again
+    scoreSection.textContent="";
+    scoreList.textContent="";
+    //resets score time and seconds remaining
+    scoreCorrect=0;
+    secondsLeft=75;
+    questionCounter=0;
+    //displays the timer and question sections
+    timeEl.style.display="block"
+    questionSection.style.display="block"
+    //calls the set time funtion
+    setTime();
+    //calls the display question function
+    displayQuestions();
+}
+
+//runs initial function
+init();
+
+
--- README.md
+++ README.md
@@ -13,13 +13,13 @@
 
 [Visit the Deployed Site]https://jamessahunter.github.io/code-quiz/
 
-The purpose of this project was to create a quiz that tests the users knowledge on some coding basics in the various technologies used to create the website in the first place. The criteria for the website was a button to start the quiz and present a timer that would decrease at a rate of one second to the user. For each question answered incorrectly the time would lose 10 seconds. The quiz ends when time reaches zero or all questions are answered. The user is then presented with their score are then allowed to enter their initials and add their score to the score board. This project taught me how to use event listeners to see if the user had clicked on a button or a specific element on the page. This also taught me how to append new elements to the page that did not already exist in the Document Object Model (DOM). This project included a built in timer function. This project taught me how to use local storage so that even if the page is refreshed that data is stored until it is deleted. 
+The purpose of this project was to create a quiz that tests the users knowledge on some coding basics in the various technologies used to create the website in the first place. The criteria for the website was a button to start the quiz and present a timer that would decrease at a rate of one second to the user. For each question answered incorrectly the time would lose 10 seconds. The quiz ends when time reaches zero or all questions are answered. The user is then presented with their score are then allowed to enter their initials and add their score to the score board. The user is also presented with a button that will alow them to replay the quiz. This project taught me how to use event listeners to see if the user had clicked on a button or a specific element on the page. This also taught me how to append new elements to the page that did not already exist in the Document Object Model (DOM). This project included a built in timer function. This project taught me how to use local storage so that even if the page is refreshed that data is stored until it is deleted. 
 
 ## Usage
 
 The user enters in their desired password length between 8 and 128 and accecpts or rejects the criteria of type of characters used. Then a password is generated and displayed on the page.
 
-![Usage Example]
+![Usage Example](/Assets/Code%20Quiz.gif)
 
 ## Learning Points
 
